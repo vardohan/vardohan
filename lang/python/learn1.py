fbba5a8ad52fface5d6edb678544241c4b0195d6
rameshbabu <vardohan.ramesh@gmail.com>
2020-10-18 22:56:40 +0530

    t-august month

---
+++ lang/python/learn1.py
@@ -0,0 +1,28 @@
+import sys
+
+dir()
+globals()
+
+print(sys.argv)
+
+value = 100.34567
+round_off = 2
+print(round(value, round_off))
+
+print(50/3) #float value
+print(50//3) #int value
+
+# Order of precedence
+"""
+1. paranthesis
+2. exponensts
+3. multiplication and division
+4. addition and subtraction
+
+PEMDAS mnemonic
+
+"""
+
+bool(1)
+bool("ramesh")
+bool("")
